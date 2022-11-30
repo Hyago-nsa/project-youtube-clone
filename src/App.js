9500743e87a05263790faa8f86445dfa821448d0
Hyago-nsa <hyago.eurico.nsa@gmail.com>
2022-11-30 15:27:22 -0300

    Navbar - Logo created
    Logo was created in the Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/system";
+
+import {
+  Navbar,
+  Feed,
+  SearchFeed,
+  ChannelDetail,
+  VideoDetail,
+} from "./components";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />}></Route>
+          <Route path="/video/:id" element={<VideoDetail />}></Route>
+          <Route path="/channel/:id" element={<ChannelDetail />}></Route>
+          <Route path="/search/:searchTerm" element={<SearchFeed />}></Route>
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
