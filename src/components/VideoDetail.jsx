20ea873af3c5b2f83009f3e99eeb7bc43cfa320b
Hyago-nsa <hyago.eurico.nsa@gmail.com>
2022-12-01 02:32:58 -0300

    VideoCard finished

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { Typography, Box, Stack } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import { Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const VideoDetail = () => {
-  return (
-    <div>VideoDetail</div>
-  )
-}
+  const [videoDetail, setVideoDetail] = useState(null);
+  const [videos, setVideos] = useState(null);
+  const { id } = useParams();
 
-export default VideoDetail
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
+      setVideoDetail(data.item[0])
+    );
+
+    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`).then(
+      (data) => setVideos(data.items)
+    );
+  }, [id]);
+
+  return (
+    <Box minHeight={"95vh"}>
+      <Stack direction={{ xs: "column", md: "row" }}>
+        <Box flex={1}>
+          <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
+            <ReactPlayer
+              url={`https://www.youtube.com/watch?v=${id}`}
+              className="react-player"
+              controls
+            />
+            <Stack
+              direction="row"
+              justifyContent={"space-between"}
+              sx={{ color: "#fff" }}
+              py={1}
+              px={2}
+            ></Stack>
+          </Box>
+        </Box>
+        <Box
+          px={2}
+          py={{ md: 1, xs: 5 }}
+          justifyContent="center"
+          alignItems={"center"}
+        >
+          <Videos videos={videos} direction="column" />
+        </Box>
+      </Stack>
+    </Box>
+  );
+};
+
+export default VideoDetail;
--- src/components/index.js
+++ src/components/index.js
@@ -6,3 +6,4 @@ export { default as VideoDetail } from "./VideoDetail";
 export { default as Sidebar } from "./Sidebar";
 export { default as Videos } from "./Videos";
 export { default as VideoCard } from "./VideoCard";
+export { default as Loader } from "./Loader";
